063fff12b501f7f53cb8a912755d1315946a2b1d
jeronimo <jerochogg2009@gmail.com>
2025-10-04 08:17:30 -0500

    Actualizar comentarios en el archivo api.ts para mayor claridad

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,7 @@
-// /services/api.ts
+// src/services/api.ts
 import axios from "axios";
 
+// URL base del backend (viene del .env.local)
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
